fafb31edfcbc51b2391a95180af2f8d6be9c6c38
serojascardenas <soniae.rojasc@gmail.com>
2021-06-10 22:10:13 -0500

    Api Rest para consumir cowsay

---
+++ index.js
@@ -0,0 +1,32 @@
+'use strict';
+
+const express = require('express');
+const cowsay = require("cowsay");
+
+// Constants
+const PORT = 8080;
+
+// App
+const app = express();
+
+app.get("/", function (req, res) {
+  res.redirect("/Módulo 0 - Curso Nvelador - Sonia Rojas!");
+});
+
+app.get("/:text", function (req, res) {
+  let text;
+
+  try {
+    text = req.params.text;
+  } catch (e) {
+    text = "Error: Módulo 0 - Curso Niveladors";
+  }
+  const responseText = `
+    <pre>${cowsay.say({ text })}</pre>
+    <br/><br/>
+  `;
+
+  res.send(responseText);
+});
+
+app.listen(PORT, () => console.log(`Running on http://localhost:${PORT}`));
